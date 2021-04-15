3a2c6d2cef55e07eca8920d9dde69d316b61fc61
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-04-15 14:26:57 +0400

    Write triangular

--- js/algorithms.js
+++ js/algorithms.js
@@ -17,3 +17,16 @@ const diffArrays = (arr1, arr2)=> {
 
     return arr3;
 }
+
+const triangArr1 = ["a", "b", "c"];
+const triangArr2 = ["c", "d", "e"];
+const arrOfTriangArrays = [];
+
+arrOfTriangArrays.push(triangArr1);
+arrOfTriangArrays.push(triangArr2);
+
+for (let i = 0; i < arrOfTriangArrays.length; i++) {
+	let current = arrOfTriangArrays[i];
+
+	console.log(current[0] + current[1] + current[2]);
+}
