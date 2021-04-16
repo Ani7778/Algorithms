f9f5e335e10f2270d055eadb4122bf806032be55
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-04-16 17:25:27 +0400

    Remove duplicates from an array

--- js/algorithms.js
+++ js/algorithms.js
@@ -62,3 +62,22 @@ const sumUp = (n)=> {
 
 	return result;
 }
+
+const arr = [1, 1, 2, 2, 3, 4, 4];
+
+const removeDupes = (arr) => {
+  	let result = [];
+  	let previous = arr[0];
+  	result[0] = previous;
+  
+  	for(let i = 0; i < arr.length; i++) {
+    
+    	if (arr[i] != previous) {
+      		result.push(arr[i]);
+    	}
+    
+    	previous = arr[i];
+  	}
+  
+  	return result;
+}
