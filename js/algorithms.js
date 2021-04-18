cff1ef7e3c7906c744d9c337ab290ee30d9722f1
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-04-18 20:30:59 +0400

    Count factorial of number

--- js/algorithms.js
+++ js/algorithms.js
@@ -94,3 +94,24 @@ const multByAdd = (a, b)=> {
 	}
 }
 
+const fact = n => {
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	return n * fact(n - 1);
+}
+
+// OR
+
+const fact2 = n => {
+	let result = 1;
+	for(i = 1; i <= n; i++) {
+		result *= i;
+	}
+
+	return result;
+}
+
+
+
