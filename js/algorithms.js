9998ab2dd0882bd3de950ab1dc99eb97cd399d38
ASUS-ПК\ASUS <aniasatryan336@gmail.com>
2021-04-18 23:45:22 +0400

    Count a number to the power of n

--- js/algorithms.js
+++ js/algorithms.js
@@ -128,6 +128,22 @@ const countWaysOfClimbingStairs = n => {
 		+ countWaysOfClimbingStairs(n - 3);
 }
 
+const powerOfNum = (x, n)=> {
+	result = 1;
 
+	for (let i = 0; i <= n; i++) {
+		result *= x;
+	}
 
+	return result;
+}
 
+// OR
+
+const powerOfNum2 = (x, n)=> {
+	if(n == 1) {
+		return x;
+	} else {
+		return x * powerOfNum2(n - 1);
+	}
+}
